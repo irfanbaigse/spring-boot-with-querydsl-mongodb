9196d75dda2c64736a536d167454ba92be1df9a5
irfanbaigse <irfan.blackhawk@gmail.com>
2021-04-26 17:50:19 +0400

    updates

--- src/main/java/com/irfanbaigse/repository/entity/User.java
+++ src/main/java/com/irfanbaigse/repository/entity/User.java
@@ -1,15 +1,20 @@
 package com.irfanbaigse.repository.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Transient;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
@@ -33,4 +38,14 @@ public class User {
     @Field(name = "updated_at")
     @LastModifiedDate
     private LocalDateTime updatedAt;
+
+    @Transient
+    @Getter(value = AccessLevel.PRIVATE)
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDate creationDateTo;
+
+    @Transient
+    @Getter(value = AccessLevel.PRIVATE)
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDate creationDateFrom;
 }
--- src/main/java/com/irfanbaigse/repository/predicate/UserPredicate.java
+++ src/main/java/com/irfanbaigse/repository/predicate/UserPredicate.java
@@ -1,12 +1,26 @@
 package com.irfanbaigse.repository.predicate;
 
 import com.irfanbaigse.repository.entity.QUser;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
+
+import java.time.LocalTime;
 
 public class UserPredicate implements QuerydslBinderCustomizer<QUser> {
     @Override
     public void customize(QuerydslBindings querydslBindings, QUser qUser) {
+
+        querydslBindings.bind(qUser.active).as("active").first(BooleanExpression::eq);
+
+        new QUser("ss").fullName.eq("anything");
+
+        querydslBindings.bind(qUser.creationDateFrom)
+                .as("created-from")
+                .first((path, value) -> qUser.createdAt.goe(value.atStartOfDay()));
+        querydslBindings.bind(qUser.creationDateTo)
+                .as("created-to")
+                .first((path, value) ->qUser.createdAt.loe(value.atTime(LocalTime.MAX)));
 
     }
 }
